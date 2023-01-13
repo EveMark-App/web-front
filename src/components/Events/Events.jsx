8998a072571a95d1179c46830d8489948ba5620d
Sami Kammoun <kammounsami77@gmail.com>
2023-01-13 02:05:18 +0100

    Retrieve Events

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -2,64 +2,45 @@ import React from "react";
 import "./events.css";
 import { BsPlusLg } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Events = () => {
-  const events = [
-    {
-      id: 1,
-      title: "Event 1",
-      short_description:
-        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages.",
-      date: "2021-09-01",
-      banner: "https://picsum.photos/2000/300",
-    },
-    {
-      id: 2,
-      title: "Event 2",
-      short_description:
-        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages. ",
-      date: "2021-09-02",
-      banner: "https://picsum.photos/2000/300",
-    },
-    {
-      id: 3,
-      title: "Event 2",
-      short_description:
-        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages. ",
-      date: "2021-09-02",
-      banner: "https://picsum.photos/2000/300",
-    },
-    {
-      id: 4,
-      title: "Event 2",
-      short_description:
-        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages. ",
-      date: "2021-09-02",
-      banner: "https://picsum.photos/2000/300",
-    },
-    {
-      id: 5,
-      title: "Event 2",
-      short_description:
-        "Summer concert series featuring local talent and delicious food. Bring blankets or chairs and enjoy the music under the stars. Family-friendly and fun for all ages. ",
-      date: "2021-09-02",
-      banner: "https://picsum.photos/2000/300",
-    },
-  ];
+  const [events, setEvents] = useState(null);
+  useEffect(() => {
+    const getEvents = async () => {
+      const response = await fetch("https://evemark.fun/api/user/get-created-events", {
+        method: "GET",
+        credentials: "include",
+      });
+      if (response.status == 200) {
+        const data = await response.json();
+        setEvents(data);
+      }
+      if (response.status === 500) {
+        return <div>Server Error</div>;
+      }
+    };
+    getEvents();
+  }, []);
+
+  if (events === null) return <div>Loading...</div>;
   return (
     <section className="events">
       <h2>Created Events</h2>
 
       <div className="events__container">
         {events.map((event) => (
-          <div key={event.id} className="event__card">
-            <div className="event__banner" style={{ backgroundImage: `url(${event.banner})` }}></div>
+          <div key={event._id} className="event__card">
+            <div className="event__banner" style={{ backgroundImage: `url(${event.bannerURL})` }}></div>
             <div className="event__details">
               <h1 className="event__title">{event.title}</h1>
               <div className="event__description">{event.short_description}</div>
               <div className="two__columns">
                 <div className="event__date">{event.date}</div>
-                <div className="btn goto">Go To</div>
+                <Link key={event._id} to={`/event/${event._id}`}>
+                  <div className="btn goto">Go To</div>
+                </Link>
               </div>
             </div>
           </div>
